dab107b3a5a8f5839df880a40e5c6452cb20b087
Алексей Волков <volkov.as@unicom24.ru>
2020-09-11 18:58:24 +0300

    fasthttp

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/beevik/etree v1.1.0
-	github.com/gorilla/mux v1.8.0
+	github.com/fasthttp/router v1.3.2
+	github.com/valyala/fasthttp v1.16.0
 	golang.org/x/text v0.3.3
 )
--- server.go
+++ server.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/beevik/etree"
-	"github.com/gorilla/mux"
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/transform"
 )
 
-func getCrv(w http.ResponseWriter, r *http.Request) {
+var branch string
+
+func getCrv(ctx *fasthttp.RequestCtx) {
 	var outStr string
-	inBody, _ := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	strBody, _ := toUtf8(string(inBody))
+	strBody, _ := toUtf8(string(ctx.Request.Body()))
 	log.Print(strings.Replace(strBody, "\n", "", -1))
 
 	doc := etree.NewDocument()
@@ -52,10 +52,10 @@ func getCrv(w http.ResponseWriter, r *http.Request) {
 	result := responseElm.CreateElement("Result")
 	result.CreateText("N")
 
-	w.Header().Set("Content-Type", "application/xml;charset=windows-1251")
+	ctx.Response.Header.Set("Content-Type", "application/xml;charset=windows-1251")
 	outStr, _ = docOut.WriteToString()
 	outResp, _ := toWin1251(outStr)
-	fmt.Fprint(w, outResp)
+	fmt.Fprint(ctx, outResp)
 }
 
 func getDate() string {
@@ -85,14 +85,43 @@ func toWin1251(text string) (s string, err error) {
 	return
 }
 
-func getStatus(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, `{"status":"ok"}`)
+func getFastCheck(ctx *fasthttp.RequestCtx) {
+	var filepath string
+	log.Print(string(ctx.RequestURI()))
+	switch string(ctx.RequestURI()) {
+	case "/ws/service?wsdl":
+		filepath = "./fastcheck.wsdl"
+	case "/ws/service?xsd":
+		filepath = "./fastcheck.xsd"
+	default:
+		ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+	}
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Print("Read file ", filepath, " ERROR", err)
+		ctx.Error("Read file error", fasthttp.StatusInternalServerError)
+	}
+	ctx.Response.Header.Set("Content-Type", "text/xml;charset=UTF-8")
+	fmt.Fprint(ctx, string(file))
 }
 
-func getVersion(w http.ResponseWriter, r *http.Request, branch string) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, `{"data": {"version": "`+branch+`"}, "error": {}}`)
+func postFastCheck(ctx *fasthttp.RequestCtx) {
+	doc := etree.NewDocument()
+	err := doc.ReadFromBytes(ctx.Request.Body())
+	if err != nil {
+		log.Print(err)
+		ctx.Error("Read request xml error", fasthttp.StatusBadRequest)
+	}
+}
+
+func getStatus(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	fmt.Fprint(ctx, `{"status":"ok"}`)
+}
+
+func getVersion(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	fmt.Fprint(ctx, `{"data": {"version": "`+branch+`"}, "error": {}}`)
 }
 
 func main() {
@@ -100,14 +129,22 @@ func main() {
 	if len(os.Getenv("GO_OTHER_MOCKS_PORT")) > 0 {
 		listenPort = os.Getenv("GO_OTHER_MOCKS_PORT")
 	}
-	var branch string
 	if len(os.Getenv("GO_OTHER_MOCKS_BRANCH")) > 0 {
 		branch = os.Getenv("GO_OTHER_MOCKS_BRANCH")
 	}
+	router := router.New()
+	router.POST("/crv", getCrv)
+	router.POST("/ws/service", postFastCheck)
+	router.GET("/ws/service", getFastCheck)
+	router.GET("/status", getStatus)
+	router.GET("/api/system/version", getVersion)
+	server := &fasthttp.Server{
+		Handler: router.Handler,
+		//		MaxRequestBodySize: 100 << 20,
+		Concurrency:      1024 * 30,
+		MaxConnsPerIP:    1024 * 10,
+		DisableKeepalive: true,
+	}
 	log.Print("App start on port ", listenPort)
-	r := mux.NewRouter()
-	r.HandleFunc("/crv", func(w http.ResponseWriter, r *http.Request) { getCrv(w, r) }).Methods("POST")
-	r.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) { getStatus(w, r) }).Methods("GET")
-	r.HandleFunc("/api/system/version", func(w http.ResponseWriter, r *http.Request) { getVersion(w, r, branch) }).Methods("GET")
-	log.Fatal(http.ListenAndServe(":"+listenPort, r))
+	log.Fatal(server.ListenAndServe(":" + listenPort))
 }
